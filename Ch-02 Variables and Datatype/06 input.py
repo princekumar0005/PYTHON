98556fc6fd1c6da356da16f862c17b4dd1937bf4
Prince Kumar <pk0834284@gmail.com>
2026-03-23 15:28:57 +0530

    DAY-2

---
+++ Ch-02 Variables and Datatype/06 input.py
@@ -0,0 +1,19 @@
+a = int(input("Enter number 1: "))
+
+
+
+b = int(input("Enter number 2: "))
+
+
+
+print("Number a is: ", a)
+
+print("Number b is: ", b) 
+
+print("Sum is ", a + b)
+
+
+
+k = input("entre your name;")
+
+print("welcome to our website;",k)
