00cf4443d57ba577db4ec505f4e17858c7285ebd
Prince Kumar <pk0834284@gmail.com>
2026-03-23 15:18:16 +0530

    Day2

---
+++ Ch-02 Variables and Datatype/02 datatype.py
@@ -0,0 +1,9 @@
+a = 8  # a is a intiger value
+
+b = 5.66 # b is a floating point number
+
+c = "i am good" # c is a string
+
+d = False/True # d is boolean variable
+
+e = None # e is none type variable
