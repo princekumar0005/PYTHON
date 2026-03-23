344ed09c94a9372e9961cfeab8e0fb4c0f3e95e0
Prince Kumar <pk0834284@gmail.com>
2026-03-23 15:22:02 +0530

    DAY-2

---
+++ Ch-02 Variables and Datatype/03 rules_variables.py
@@ -0,0 +1,18 @@
+a = 23
+
+
+aaa = 435
+
+
+harry = 34
+
+
+sameer = 45
+
+
+_samerr = 34
+
+
+# @sameer = 56 # Invalid due to @ symbol
+# s@meer # Invalid due to @ symbol
+# 1sanndnc = kis # Invalid due to 1 symbol
