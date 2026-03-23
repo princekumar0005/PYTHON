0c0da26a7da2602ae4cf6b4593fa5b4a4e406052
Prince Kumar <pk0834284@gmail.com>
2026-03-23 15:32:50 +0530

    DAY-2

---
+++ Ch-02 Variables and Datatype/ps_ch-2.py
@@ -0,0 +1,48 @@
+# write a python program to add two number.
+
+a = 45
+b= 55 
+
+print(a+b)
+
+# . Write a python program to find remainder when a number is divided by z.
+
+a = 34 
+
+b = 5
+
+print("Remainder when a is divided by b is ", a%b)
+
+# Check the type of variable assigned using input () function.
+
+a = input(" enter the value of a;")
+
+print(type(a))
+
+# Use comparison operator to find out whether ‘a’ given variable a is greater than‘b’ or not. Take a = 34 and b = 80
+
+a=34
+b=80
+
+k=a>=b
+print(k)
+
+# Write a python program to find an average of two numbers entered by the user.
+
+a = int(input("Enter the number 1;"))
+b = int(input("Enter the number 2;"))
+
+print("The avrege of the number is",(a+b)/2)
+
+# Write a python program to calculate the square of a number entered by the user.
+
+a = int(input("Enter the number ;"))
+
+print("The squer of the number is ;",(a*a))
+
+# write a program to input two int number a and b print True if a is greater then or egual to b if not print False.
+
+a = int(input("Enter the number 1 ;"))
+b = int(input("Enter the number 2 ;"))
+print(" a is greter then or equal to  b;",a>=b)
+
