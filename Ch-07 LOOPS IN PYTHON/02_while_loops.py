ddde7f089b3d7191fb7681a878512881ca0d4561
Prince Kumar <pk0834284@gmail.com>
2026-03-28 13:13:47 +0530

    DAY-7

---
+++ Ch-07 LOOPS IN PYTHON/02_while_loops.py
@@ -0,0 +1,23 @@
+i = 1
+
+while(i<6):
+    print(i)
+    i +=1 # or i = i + 1
+'''
+Output:
+1
+2
+3
+4
+5
+'''
+
+
+# Quick Quiz: Write a program to print 1 to 50 using a while loop
+
+i=1
+
+while(i<51):
+    print(i)
+    i+=1
+    
