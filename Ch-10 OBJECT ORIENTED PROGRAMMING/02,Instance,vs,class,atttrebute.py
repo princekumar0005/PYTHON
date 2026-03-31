aa0d4553a5c04ace821bda4daffd00fc6c6b0064
Prince Kumar <pk0834284@gmail.com>
2026-03-31 07:56:37 +0530

    Add Employee class with attributes and instance example

---
+++ Ch-10 OBJECT ORIENTED PROGRAMMING/02,Instance,vs,class,atttrebute.py
@@ -0,0 +1,13 @@
+class Employee:
+
+    language = " py" # This is a class attribute
+
+    salary = 120000000 
+
+
+
+prince = Employee()
+
+prince.language = "Javascript" # THis is an instance attribute
+
+print(prince.salary, prince.language)
