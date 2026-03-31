d533b936fe5f3fb64dd88c9304eea6f6c39af6c1
Prince Kumar <pk0834284@gmail.com>
2026-03-31 07:55:51 +0530

    Create Employee class with attributes and instances
    Define Employee class with class and instance attributes.

---
+++ Ch-10 OBJECT ORIENTED PROGRAMMING/01,CLASS.py
@@ -0,0 +1,25 @@
+class Employee:
+
+    language = " py" # This is a class attribute
+
+    salary = 120000000 
+
+
+
+prince = Employee()
+
+prince.name = "Prince" # THis is an instance attribute
+
+print(prince.name,prince.salary, prince.language)
+
+
+
+rohan= Employee()
+
+rohan.name= "rohan robin"
+
+print(rohan.name,rohan.salary, rohan.language)
+
+
+
+# Here name is instance attribute and salary and language are class attribute as they directly belong to the class
