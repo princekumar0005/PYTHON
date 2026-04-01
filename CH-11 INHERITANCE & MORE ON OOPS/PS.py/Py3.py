1ea219399db8c0c8115f5413e0eecace6874dd0b
Prince Kumar <pk0834284@gmail.com>
2026-04-01 10:45:30 +0530

    Define Employee class with salary and increment
    Add salary and increment properties to Employee class.

--- CH-11 INHERITANCE & MORE ON OOPS/PS.py/Py3.py
+++ CH-11 INHERITANCE & MORE ON OOPS/PS.py/Py3.py
@@ -1,5 +1,7 @@
 # 3. Create a class ‘Employee’ and add salary and increment properties to it.
+#Write a method ‘salaryAfterIncrement’ method with a @property decorator with a setter 
 
+which changes the value of increment based on the salary.
 class Employee:
     salary = 234
     increment = 20 
